9b9cfad904eaa409c7c1a1f810109e58acab4929
cgy <1020110023@qq.com>
2018-06-07 15:18:56 +0800

    会话列表adapter各个界面完成，哇！太费劲

---
+++ weixin/src/main/java/com/cgy/weixin/widget/BubbleImageView.java
@@ -0,0 +1,8 @@
+package com.cgy.weixin.widget;
+
+/**
+ * Created by cgy
+ * 2018/6/7  10:32
+ */
+public class BubbleImageView {
+}
---
+++ weixin/src/main/java/com/cgy/weixin/widget/CircularProgressBar.java
@@ -0,0 +1,8 @@
+package com.cgy.weixin.widget;
+
+/**
+ * Created by cgy
+ * 2018/6/7  13:55
+ */
+public class CircularProgressBar {
+}
