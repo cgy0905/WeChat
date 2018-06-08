3b1178dcd97c6dc9488071e15749980db3fb70d4
cgy <1020110023@qq.com>
2018-06-08 18:18:04 +0800

    集成shareSdk分享功能

---
+++ weixin/src/main/java/com/cgy/weixin/apshare/ShareEntryActivity.java
@@ -0,0 +1,7 @@
+package com.cgy.weixin.apshare;
+
+import cn.sharesdk.alipay.utils.AlipayHandlerActivity;
+
+public class ShareEntryActivity extends AlipayHandlerActivity {
+
+}
---
+++ weixin/src/main/java/com/cgy/weixin/widget/ProgressWebView.java
@@ -0,0 +1,8 @@
+package com.cgy.weixin.widget;
+
+/**
+ * Created by cgy
+ * 2018/6/8  16:53
+ */
+public class ProgressWebView {
+}
